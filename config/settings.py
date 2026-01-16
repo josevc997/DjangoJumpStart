9bbaf2a3a141951387c0a0693e3a5e914be3e50f
Jose Valencia <19josevalencia97@gmail.com>
2026-01-16 11:41:50 +0000

    Update dependencies and add favicon
    - Updated Python version to 3.12 and Django to 6.0.1.
    - Upgraded various dependencies including psycopg, djangorestframework, and others to their latest compatible versions.
    - Added a favicon.ico file to the static directory.
    - Created a custom admin base template to include the favicon in the admin interface.

--- config/settings.py
+++ config/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],  # Ensure this exists
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -137,8 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_URL = "media/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path(
+        "favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))
+    ),
     path("admin/", admin.site.urls),
     path("api/users/", include("apps.user.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
